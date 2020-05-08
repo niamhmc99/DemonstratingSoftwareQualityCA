4f596c954808b5de557d19408e44ada4929f3a59
niamhmc99 <niamhmc99@gmail.com>
2020-05-08 20:36:34 +0100

    Create Survey Test Passing

---
+++ survey/src/main/java/com/example/survey/java/com/example/survey/Survey/SurveyController.java
@@ -0,0 +1,30 @@
+package com.example.survey.java.com.example.survey.Survey;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class SurveyController {
+    private SurveyService surveyService;
+
+    public SurveyController(SurveyService surveyService) {
+        this.surveyService = surveyService;
+    }
+
+    @RequestMapping(value="/survey", method=RequestMethod.POST)
+    public void createSurvey(@RequestParam Survey survey) {
+            surveyService.createSurvey(survey);
+        }
+
+    @RequestMapping("/surveys/{surveyName}")
+	public Survey getSurvey(@PathVariable("name") String surveyName) {
+        return surveyService.getSurvey(surveyName);
+	}
+    
+
+
+}
---
+++ survey/src/test/java/com/example/survey/SurveyTest.java
@@ -0,0 +1,20 @@
+package com.example.survey;
+
+import com.example.survey.java.com.example.survey.Survey.Survey;
+import com.example.survey.java.com.example.survey.Survey.SurveyController;
+import com.example.survey.java.com.example.survey.Survey.SurveyService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class SurveyTest {
+
+    SurveyController surveyController = new SurveyController(new SurveyService());
+
+    @Test
+    void createSurveyTest(){
+        surveyController.createSurvey(new Survey(22, "Test to Create Survey"));
+		Survey surveyTest = surveyController.getSurvey("Test to Create Survey");
+       assertEquals(surveyTest.getId(), 22, "messageSupplier");
+    }
+}
