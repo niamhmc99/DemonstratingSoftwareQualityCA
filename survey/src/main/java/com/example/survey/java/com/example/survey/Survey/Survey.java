d43993af6a5e2c24efe6e36c6cbcbd0f2145e63d
niamhmc99 <niamhmc99@gmail.com>
2020-05-08 14:16:09 +0100

    Created Survey and Survey Response Model

---
+++ survey/src/main/java/com/example/survey/java/com/example/survey/Survey/Survey.java
@@ -0,0 +1,49 @@
+package com.example.survey.java.com.example.survey.Survey;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.survey.java.com.example.survey.SurveyResponse.SurveyResponse;
+
+import org.springframework.lang.NonNull;
+
+public class Survey {
+
+    @NonNull
+    private int id;
+    @NonNull
+    private String surveyName;
+   @NonNull
+   private List<SurveyResponse> responses;
+
+   public Survey(int id, String surveyName) {
+        this.id = id;
+        this.surveyName = surveyName;
+        this.responses = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSurveyName() {
+        return surveyName;
+    }
+
+ 
+    public void setSurveyName(String surveyName) {
+        this.surveyName = surveyName;
+    }
+
+    public List<SurveyResponse> getResponses() {
+        return responses;
+    }
+
+    public void setResponses(List<SurveyResponse> responses) {
+        this.responses = responses;
+    }
+}
---
+++ survey/src/main/java/com/example/survey/java/com/example/survey/SurveyResponse/SurveyResponse.java
@@ -0,0 +1,43 @@
+package com.example.survey.java.com.example.survey.SurveyResponse;
+
+import java.util.List;
+
+import org.springframework.lang.NonNull;
+
+public class SurveyResponse {
+
+    @NonNull
+    private int surveyResponseId;
+    @NonNull //answer is between 1-5
+    private int responseAnswer;
+    @NonNull
+    private List<Integer> responses;
+
+    public int getSurveyResponseId() {
+        return surveyResponseId;
+    }
+
+    public void setSurveyResponseId(int surveyResponseId) {
+        this.surveyResponseId = surveyResponseId;
+    }
+
+    public int getResponseAnswer() {
+        return responseAnswer;
+    }
+
+    public void setResponseAnswer(int responseAnswer) {
+        this.responseAnswer = responseAnswer;
+    }
+
+    public List<Integer> getResponses() {
+        return responses;
+    }
+
+    public void setResponses(List<Integer> responses) {
+        this.responses = responses;
+    }
+
+    public void addResponses (int responseAnswer){
+        this.responses.add(responseAnswer);
+    }
+}
